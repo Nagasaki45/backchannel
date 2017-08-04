a87432abf7d28e3fb0c5515e02b7ba049b40ac12
Tom Gurion <tgurion@inition.co.uk>
2017-08-04 16:03:45 +0100

    Fine tune the backchannel threshold scaling parameters

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ BEHAVIOURS = ('SpeechSilent', )
 # Each second the de Kok threshold decreases to x of it's previous value
 DEKOK_DECREASE = 0.9
 FIGSIZE = (10, 6)
-MAX_THRESHOLD = 0.7
+MAX_THRESHOLD = 0.5
 MIN_THRESHOLD = 0.3
